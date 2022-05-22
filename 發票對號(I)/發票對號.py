8fd9c7971f77da9b242949a8af8d117ad9b19bb6
XUPGT <owen20060101@gmail.com>
2022-05-22 11:36:24 +0800

    Update 發票對號.py

--- 發票對號(I)/發票對號.py
+++ 發票對號(I)/發票對號.py
@@ -1,6 +1,9 @@
 win = open('中獎號碼.txt').read().split()
 mine = open('我的號碼.txt').read().split()
+cnt = 0
 
 for i, num in enumerate(mine):
     if num in win:
         print('第', i+1, '張發票對中號碼', num, '!')
+
+print('有', cnt, '張發票中了!')
